f52daa2ff0f24cf2f4fe109a6c06b29f86c0be21
AleksiDu <aleksi.duluzauri@gmail.com>
2023-11-16 14:44:03 +0400

    add scan

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self,Write};
+use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+
+
+const MAX:u16 = 65535;
 
 struct Arguments{
     flag:String,
@@ -44,6 +51,24 @@ impl Arguments {
     }
 }
 
+fn scan(tx: Sender<u16>, start_port:u16, addr: IpAddr, num_threads:u16){
+    let mut port:u16 = start_port + 1;
+    loop{
+        match TcpStream::connect((addr,port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+        if(MAX - port) <= num_threads{
+            break;
+        }
+        port += num_threads;
+    }
+}
+
 fn main() {
     let args:Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -52,9 +77,20 @@ fn main() {
             if err.contains("help"){
                 process::exit(0)
             } else {
-                eprintln!("{} problem parsing arguments: {}", program, err)
-                process::exit(0)
+                eprintln!("{} problem parsing arguments: {}", program, err);
+                process::exit(0);
             }
         }
-    )
+    );
+
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let(tx,rx) = channel();
+    for i in 0..num_threads{
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i , addr, num_threads);
+        });
+    }
 }
