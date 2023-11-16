d72e92fc232c4a1a4168714872787b1bf64aab15
AleksiDu <aleksi.duluzauri@gmail.com>
2023-11-16 14:28:06 +0400

    implement block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::env;
+use std::net::IpAddr;
+use std::str::FromStr;
+use std::process;
+
+struct Arguments{
+    flag:String,
+    ipaddr:IpAddr,
+    threads:u16,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        if args.len()<2{
+            return  Err("not enough arguments");
+        } else if args.len() > 4{
+            return  Err("too many arguments");
+        }
+        let f = args[1].clone();
+        if let Ok(ipaddr) = IpAddr::from_str(&f){
+            return Ok(Arguments { flag: String::from(""), ipaddr, threads: 4 });
+        } else {
+            let flag = args[1].clone();
+            if flag.contains("-h") || flag.contains("--help") && args.len() == 2 {
+                println!("Usage: -j to selected how many threads you want
+                \r\n      -h or --help tp show this help message");
+                return  Err("help");
+            } else if flag.contains("-h") || flag.contains("--help") {
+                return  Err("too many arguments");
+            } else if flag.contains("-j"){
+                let ipaddr  = match IpAddr::from_str(&args[3]){
+                    Ok(s)=>s,
+                    Err(_)=> return Err("not a valid IPADDR; mast be IPv4 or IPv6"),
+                };
+                let threads = match args[2].parse::<u16>(){
+                   Ok(s) => s,
+                   Err(_) => return  Err("failde to parse thread number")
+                };
+                return Ok(Arguments { flag, ipaddr, threads });
+            } else {
+                return Err("invalid syntax");
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let arguments = Arguments::new(&args).unwrap_or_else(
+        |err| {
+            if err.contains("help"){
+                process::exit(0)
+            } else {
+                eprintln!("{} problem parsing arguments: {}", program, err)
+                process::exit(0)
+            }
+        }
+    )
 }
